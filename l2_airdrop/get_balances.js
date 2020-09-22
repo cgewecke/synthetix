65fed0a4f1754562e579d9c8aac4feac8d14df81
i-stam <i.stamelakos@gmail.com>
2020-09-22 15:26:14 +0100

    Edit script so it returns all the stakers eligible for aidrop

--- l2_airdrop/get_balances.js
+++ l2_airdrop/get_balances.js
@@ -4,14 +4,15 @@ const snxData = require('synthetix-data');
 
 const JSON_OUTPUT = 'snxStakers2500.json';
 
-snxData.snx.holders({ max: 10 }).then(holders => {
+// max needs to be a very big number, the only way to solve this is to edit synthetix-data and deactivate the default value or use ascending order
+snxData.snx.holders({ max: 25000 }).then(holders => {
 	const newHolders = holders
-		.filter(({ collateral }) => collateral > 2500 && collateral >= 1)
+		.filter(({ collateral }) => collateral <= 2500 && collateral >= 1)
 		.map(({ address, collateral }) => ({
 			address: address,
 			collateral: collateral,
 		}));
-	// console.log(newHolders);
+	console.log(newHolders.length);
 	fs.writeFile(JSON_OUTPUT, JSON.stringify(newHolders), 'utf8', err => {
 		if (err) {
 			console.log('An error occured while writing JSON Object to File.');
@@ -22,10 +23,10 @@ snxData.snx.holders({ max: 10 }).then(holders => {
 	});
 });
 
-fs.readFile(JSON_OUTPUT, (err, data) => {
-	if (err) throw err;
-	const stakers = JSON.parse(data);
-	stakers.forEach(staker => {
-		console.log(staker.address);
-	});
-});
+// fs.readFile(JSON_OUTPUT, (err, data) => {
+// 	if (err) throw err;
+// 	const stakers = JSON.parse(data);
+// 	stakers.forEach(staker => {
+// 		console.log(staker.address);
+// 	});
+// });
